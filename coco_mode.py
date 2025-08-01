26dc847a8d8e0ab22cd03d822acd688a6c67004f
Draymoor <iris250100@gmail.com>
2025-08-01 20:20:15 +0300

    Коррекция создания Yaml-файла

--- coco_mode.py
+++ coco_mode.py
@@ -25,11 +25,13 @@ def coco_mode(images_folder:str, classes_indexes: list[int]):
     # rename_images(images_folder) # Переименование изображений
     coco_annotate_it(input_dir=images_folder, output_dir=texts_folder)  # Создание текстовых файлов
     replace_first_number_in_files(texts_folder, classes_indexes)
-    augmentate_it(dir_name_images=[images_folder], dir_name_textes=[texts_folder])
+    augmentate_it(dir_name_images=images_folder, dir_name_textes=texts_folder)
+    '''
     dataset_name = 'dataset'
     create_empty_dataset(dataset_name)
     fill_the_dataset(dataset_name, images_folder, texts_folder)
     create_yaml(dataset_name, classes_indexes, COCO_dataset_info)
+    '''
 
 if __name__ == '__main__':
     #t1 = time.time()
